d8f4cc3ec28ea89b6e140b56728588f73f62c891
LeGnD2 <Liongaming386@gmail.com>
2025-05-19 20:18:57 +0200

    2

--- pages/login-form.php
+++ pages/login-form.php
@@ -1,4 +1,4 @@
-<?php require "includes/header.php" ?>
+<?php require "../includes/header.php" ?>
 <main>
     <form action="/login-handler" class="account-form" method="post">
         <h2>Log in</h2>
