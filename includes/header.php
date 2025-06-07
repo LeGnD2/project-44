1d13f9af6c41b825bd57cd12a7ad68d899e8d872
LeGnD2 <Liongaming386@gmail.com>
2025-06-07 22:40:55 +0200

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -7,7 +7,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Rydr</title>
-    <link rel="stylesheet" href="assets/css/main.css">
+    <link rel="stylesheet" href="/assets/css/main.css">
     <link rel="icon" type="image/png" href="assets/images/favicon.ico" sizes="32x32">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
